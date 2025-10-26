7b740e1839c148bbd7a195275ad6b5f6d9237c4a
DmitryLatyshko <dmitry.latyshko@outlook.com>
2025-10-26 21:59:31 +0200

    #add json of duties, update duties list

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	startSprint: 153,
-	startDate: '2025-09-01',
-	dutiesFileProviderName: 'xlsx',
+	startSprint: 157,
+	startDate: '2025-10-27',
+	dutiesFileProviderName: 'json',
 	messageType: 'MessageCard',
 	webhookUrl:
 		'https://terrasoftua.webhook.office.com/webhookb2/8a367305-fea1-4881-8e39-b8f304821100@7b5ca2fb-a643-4385-8b39-36934270c716/IncomingWebhook/50f9346b137f46b98d4318a51202742d/7436fc07-f948-4ce2-9214-1c3a15a4a72b/V2fktToig0ygb685zPiYVNqyistQWwXotPGeZym0vBnWw1',
